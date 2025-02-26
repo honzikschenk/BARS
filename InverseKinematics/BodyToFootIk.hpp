061121ebc63092a131ac9a5631f5a4ba981f1bdc
honzikschenk <nojschenk@gmail.com>
2025-02-25 19:37:30 -0500

    Add initial BodyToFootIK implementation and update StateManager submodule

---
+++ InverseKinematics/BodyToFootIk.hpp
@@ -0,0 +1,16 @@
+/**
+ * @brief Calculates the inverse kinematics to move a foot to a specific coordinate relative to the body.
+ * @author Honzik Schenk
+ * 
+ * BodyToFootIK is a library that calculates the inverse kinematics necessary to move a foot to a specific
+ * coordinate relative to the center of the body. The library goes off the assumption that there are two
+ * legs and each have a 3-axis hip joint, a 1-axis knee joint, and a 1-axis ankle joint. The library also
+ * assumes that the legs are symmetrical.
+ */
+
+#ifndef BODYTOFOOTIK_HPP
+#define BODYTOFOOTIK_HPP
+
+
+
+#endif // BODYTOFOOTIK_HPP
